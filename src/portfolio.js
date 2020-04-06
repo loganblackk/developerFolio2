ae65370ffdc9c1878cecec199cfa17ec68ee0e73
Michael Logan Foy <loganblack0@gmail.com>
2020-04-05 21:14:55 -0700

    edited some of portfolio js

--- src/portfolio.js
+++ src/portfolio.js
@@ -35,221 +35,233 @@ const skillsSection = {
   /* Make Sure You include correct Font Awesome Classname to view your icon
   https://fontawesome.com/icons?d=gallery */
 
-  softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "github",
-      fontAwesomeClassname: "fab fa-github"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "mongodb",
-      fontAwesomeClassname: "fab fa-mdb"
-    },
-    {
-      skillName: "bootstrap",
-      fontAwesomeClassname: "fab fa-bootstrap"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "php",
-      fontAwesomeClassname: "fab fa-php"
-    },
-    {
-      skillName: "angular",
-      fontAwesomeClassname: "fab fa-angular"
-    }
-  ]
-};
+  const skillsSection = {
+    title: "What I do",
+    subTitle: "PASSIONATE FULL STACK DEVELOPER",
+    skills: [
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+      "⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks",
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    ],
 
-// Your top 3 proficient stacks/tech experience
+    /* Make Sure You include correct Font Awesome Classname to view your icon
+    https://fontawesome.com/icons?d=gallery */
 
-const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
-  experience: [
-    {
-      Stack: "Frontend/Design",  //Insert stack or technology you have experience in
-      progressPercentage: "90%"  //Insert relative proficiency in percentage
-    },
-    {
-      Stack: "Backend",
-      progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
-  ]
-};
+    softwareSkills: [
+      {
+        skillName: "html-5",
+        fontAwesomeClassname: "fab fa-html5"
+      },
+      {
+        skillName: "css3",
+        fontAwesomeClassname: "fab fa-css3-alt"
+      },
+      {
+        skillName: "sass",
+        fontAwesomeClassname: "fab fa-sass"
+      },
+      {
+        skillName: "JavaScript",
+        fontAwesomeClassname: "fab fa-js"
+      },
+      {
+        skillName: "reactjs",
+        fontAwesomeClassname: "fab fa-react"
+      },
+      {
+        skillName: "nodejs",
+        fontAwesomeClassname: "fab fa-node"
+      },
+      {
+        skillName: "github",
+        fontAwesomeClassname: "fab fa-github"
+      },
+      {
+        skillName: "npm",
+        fontAwesomeClassname: "fab fa-npm"
+      },
+      {
+        skillName: "sql-database",
+        fontAwesomeClassname: "fas fa-database"
+      },
+      {
+        skillName: "aws",
+        fontAwesomeClassname: "fab fa-aws"
+      },
+      {
+        skillName: "mongodb",
+        fontAwesomeClassname: "fab fa-mdb"
+      },
+      {
+        skillName: "bootstrap",
+        fontAwesomeClassname: "fab fa-bootstrap"
+      },
+      {
+        skillName: "firebase",
+        fontAwesomeClassname: "fas fa-fire"
+      },
+      {
+        skillName: "python",
+        fontAwesomeClassname: "fab fa-python"
+      },
+      {
+        skillName: "php",
+        fontAwesomeClassname: "fab fa-php"
+      },
+      {
+        skillName: "angular",
+        fontAwesomeClassname: "fab fa-angular"
+      }
+    ]
+  };
 
-/* Your Open Source Section to View Your Github Pinned Projects
-To know how to get github key look at readme.md */
+  // Your top 3 proficient stacks/tech experience
 
-const openSource = {
-  githubConvertedToken: "ZGU2NTBiYzM2YWYyYjE0M2ExNDhhYWM3Yzg5ZDIxZjBjY2RiMTFhNw==",
-  githubUserName: "loganblackk"
-};
+  const techStack = {
+    viewSkillBars: true, //Set it to true to show Proficiency Section
+    experience: [
+      {
+        Stack: "Frontend/Design",  //Insert stack or technology you have experience in
+        progressPercentage: "90%"  //Insert relative proficiency in percentage
+      },
+      {
+        Stack: "Backend",
+        progressPercentage: "70%"
+      },
+      {
+        Stack: "Programming",
+        progressPercentage: "60%"
+      }
+    ]
+  };
+
+  /* Your Open Source Section to View Your Github Pinned Projects
+  To know how to get github key look at readme.md */
+
+  const openSource = {
+    githubConvertedToken: "ZGU2NTBiYzM2YWYyYjE0M2ExNDhhYWM3Yzg5ZDIxZjBjY2RiMTFhNw==",
+    githubUserName: "loganblackk"
+  };
 
 
-// Some Big Projects You have worked with your company
+  // Some Big Projects You have worked with your company
 
-const bigProjects = {
-  title: "Projects",
-  subtitle: "SOME WEBSITES AND APPS I'VE CREATED",
-  projects: [
-    {
-      image: "http://boxofficequant.com/wp-content/uploads/2011/04/Film-Seasons-All.png",
-      link: "https://github.com/loganblackk/ProjectTwo"
-    },
-    {
-      image: "https://www.codester.com/static//uploads/items/3290/preview-xl.jpg",
-      link: "https://github.com/loganblackk/wdi-fundamentals-memorygame"
-    }
-  ]
-};
+  const bigProjects = {
+    title: "Projects",
+    subtitle: "SOME WEBSITES AND APPS I'VE CREATED",
+    projects: [
+      {
+        image: "http://boxofficequant.com/wp-content/uploads/2011/04/Film-Seasons-All.png",
+        link: "https://github.com/loganblackk/ProjectTwo"
+      },
+      {
+        image: "https://www.codester.com/static//uploads/items/3290/preview-xl.jpg",
+        link: "https://github.com/loganblackk/wdi-fundamentals-memorygame"
+      }
+    ]
+  };
 
-// Your Achievement Section Include Your Certification Talks and More
+  // Your Achievement Section Include Your Certification Talks and More
 
-const achievementSection = {
+  const achievementSection = {
 
-  title: "Achievements And Certifications 🏆 ",
-  subtitle: "Achievements, Certifications and Some Cool Stuff that I have done!",
+    title: "Achievements And Certifications 🏆 ",
+    subtitle: "Achievements, Certifications Award Letters and Some Cool Stuff that i have done !",
 
-  // achivementsCards: [
-  //   {
-  //     title: "Google Code-In Finalist",
-  //     description: "First Pakistani to be selected as Google Google-in Finalist from 4000 students from 77 diffrent countries",
-  //     image:
-  //       "https://1.bp.blogspot.com/-Ig-v1tDXZt4/XDODmZvWp1I/AAAAAAAAB0A/KtbFdBPFVQw2O15FekkIR0Yg8MUp--rngCLcBGAs/s1600/GCI%2B-%2BVertical%2B-%2BGray%2BText%2B-%2BWhite%2BBG.png",
-  //     footerLink: [
-  //       { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
-  //       { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
-  //       { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
-  //     ]
-  //   },
-  //   {
-  //     title: "Google Assistant Action",
-  //     description: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-  //     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
-  //     footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
-  //   },
+    achivementsCards: [
+      {
+        title: "Google Code-In Finalist",
+        description: "First Pakistani to be selected as Google Google-in Finalist from 4000 students from 77 diffrent countries",
+        image:
+          "https://1.bp.blogspot.com/-Ig-v1tDXZt4/XDODmZvWp1I/AAAAAAAAB0A/KtbFdBPFVQw2O15FekkIR0Yg8MUp--rngCLcBGAs/s1600/GCI%2B-%2BVertical%2B-%2BGray%2BText%2B-%2BWhite%2BBG.png",
+        footerLink: [
+          { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
+          { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
+          { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
+        ]
+      },
+      {
+        title: "Google Assistant Action",
+        description: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
+        footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
+      },
 
-  //   {
-  //     title: "PWA Web App Developer",
-  //     description: "Completed Certifcation from SMIT for PWA Web App Development",
-  //     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu93Bd6LDbxPEOXr-hfLqLYzmHny8c0MJoI3exQP-lwpFLRT7g&s",
-  //     footerLink: [
-  //       { name: "Certification", url: "" },
-  //       { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
-  //     ]
-  //   }
-  // ]
-};
+      {
+        title: "PWA Web App Developer",
+        description: "Completed Certifcation from SMIT for PWA Web App Development",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu93Bd6LDbxPEOXr-hfLqLYzmHny8c0MJoI3exQP-lwpFLRT7g&s",
+        footerLink: [
+          { name: "Certification", url: "" },
+          { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
+        ]
+      }
+    ]
+  };
 
-// Blogs Section
+  // Blogs Section
 
-const blogSection = {
+  const blogSection = {
 
-  title: "Blogs",
-  subtitle: "With Love for Developing cool stuff. I love to write and teach others what I've learned",
+    title: "Blogs",
+    subtitle: "With Love for Developing cool stuff. i love to write and teach others what i have learned",
 
-  blogs: [
-    {
-      url: "https://medium.com/@loganblack0/how-to-get-a-job-in-the-tech-industry-without-ever-leaving-your-house-6ed2586b888a",
-      image: "https://miro.medium.com/max/505/1*5A4XAHkOO_Hl2aYglkyriQ.jpeg",
-      title: "How to get a job in the tech industry without ever leaving your house",
-      description: "Want to make a career pivot into a field that’s future proof, in which you pay nothing until you land a job in the industry? Join a reputable coding boot camp that shows their proven track record of high job placement outcomes for their cohorts."
-    }
-    // {
-    //   url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-    //   image: "https://miro.medium.com/max/1280/1*o08b2PW-1PVs94riZuhNMA.png",
-    //   title: "Why REACT Is The Best?",
-    //   description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
-    // }
-  ]
-};
+    blogs: [
+      {
+        url: "https://medium.com/@loganblack0/how-to-get-a-job-in-the-tech-industry-without-ever-leaving-your-house-6ed2586b888a",
+        image: "https://miro.medium.com/max/505/1*5A4XAHkOO_Hl2aYglkyriQ.jpeg",
+        title: "How to get a job in the tech industry without ever leaving your house",
+        description: "Want to make a career pivot into a field that’s future proof, in which you pay nothing until you land a job in the industry? Join a reputable coding boot camp that shows their proven track record of high job placement outcomes for their cohorts."
+      },
+      {
+        url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+        image: "https://miro.medium.com/max/1280/1*o08b2PW-1PVs94riZuhNMA.png",
+        title: "Why REACT Is The Best?",
+        description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
+      }
+    ]
+  };
 
-// Talks Sections
+  // Talks Sections
 
-// const talkSection = {
-//   title: "TALKS",
-//   subtitle: "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅",
+  const talkSection = {
+    title: "TALKS",
+    subtitle: "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅",
 
-//   talks: [
-//     {
-//       title: "Build Actions For Google Assistant",
-//       subtitle: "Codelab at GDG DevFest Karachi 2019",
-//       slides_url: "https://bit.ly/saadpasta-slides",
-//       event_url: "https://www.facebook.com/events/2339906106275053/",
-//       image:
-//         "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/76714032_1730516240415559_1293494289556307968_o.jpg?_nc_cat=103&_nc_ohc=s5f81rdZd6wAQmqUa52YQBA21MjVOy7e-HN9jI7MspRDC-v5ahEouyInQ&_nc_ht=scontent.fkhi6-1.fna&oh=c29141a9a0de7ff2011a8191f5475a78&oe=5E83127C"
-//     }
-//   ]
-// };
+    talks: [
+      {
+        title: "Build Actions For Google Assistant",
+        subtitle: "Codelab at GDG DevFest Karachi 2019",
+        slides_url: "https://bit.ly/saadpasta-slides",
+        event_url: "https://www.facebook.com/events/2339906106275053/",
+        image:
+          "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/76714032_1730516240415559_1293494289556307968_o.jpg?_nc_cat=103&_nc_ohc=s5f81rdZd6wAQmqUa52YQBA21MjVOy7e-HN9jI7MspRDC-v5ahEouyInQ&_nc_ht=scontent.fkhi6-1.fna&oh=c29141a9a0de7ff2011a8191f5475a78&oe=5E83127C"
+      }
+    ]
+  };
 
-// Podcast Section
+  // Podcast Section
 
-const podcastSection = {
-  title: "Podcast 🎙️",
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY"
+  const podcastSection = {
+    title: "Podcast 🎙️",
+    subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
 
-  // Please Provide with Your Podcast embedded Link
-  // podcast: ["https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"]
-};
+    // Please Provide with Your Podcast embeded Link
+    podcast: ["https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"]
+  };
 
-const contactInfo = {
-  title: "Contact Me ☎️",
-  subtitle: "To discuss a project or a job offer, or you if just want to say hi my inbox is open for all",
-  number: "(714) 202 - 6331",
-  email_address: "michaelfoy870@gmail.com"
-};
+  const contactInfo = {
+    title: "Contact Me ☎️",
+    subtitle: "To discuss a project or a job offer, or you if just want to say hi my inbox is open for all",
+    number: "(714) 202 - 6331",
+    email_address: "michaelfoy870@gmail.com"
+  };
 
-//Twitter Section
+  //Twitter Section
 
-const twitterDetails = {
+  const twitterDetails = {
 
-  userName: "FullStackLogan"//Replace "twitter" with your twitter username without @
+    userName: "FullStackLogan"//Replace "twitter" with your twitter username without @
 
-};
-export { greeting, socialMediaLinks, skillsSection, techStack, openSource, bigProjects, achievementSection, blogSection, podcastSection, contactInfo, twitterDetails };
+  };
+  export { greeting, socialMediaLinks, skillsSection, techStack, openSource, bigProjects, achievementSection, blogSection, talkSection, podcastSection, contactInfo, twitterDetails };
