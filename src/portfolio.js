23602aa1e098fe84692a2362362571fd993ca072
Michael Logan Foy <loganblack0@gmail.com>
2020-04-05 21:03:53 -0700

    trying again

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,7 +4,7 @@
 
 const greeting = {
   /* Your Summary And Greeting Section */
-  title: "Hi all I'm Saaad",
+  title: "Hi all I'm Saaaad",
   subTitle:
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks",
   resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
